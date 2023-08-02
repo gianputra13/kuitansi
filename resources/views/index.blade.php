531402900976543dd1e16af2e46e93b9b5abd3f5
gianputra13 <gianputra131@gmail.com>
2023-08-02 13:29:58 +0700

    update tabel

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -21,30 +21,29 @@
       </div>
       <div class="col-span-12 md:col-span-10 lg:col-start-4">
           <!-- Tabel -->
-        <div class="overflow-x-auto rounded-lg shadow-lg">
-            <table class="w-full border-collapse bg-white">
-              <thead>
-                  <tr class="bg-gray-100">
-                      <th class="border-b border-gray-300 px-4 py-2">Kode</th>
-                      <th class="border-b border-gray-300 px-4 py-2">Diterima Dari</th>
-                      <th class="border-b border-gray-300 px-4 py-2">Nominal</th>
-                  </tr>
-              </thead>
-              <tbody>
+          <table class="w-full border-collapse bg-white">
+            <thead>
+                <tr class="bg-gray-100">
+                    <th class="border-b border-gray-300 px-3 py-2">Kode</th>
+                    <th class="border-b border-gray-300 px-3 py-2">Diterima Dari</th>
+                    <th class="border-b border-gray-300 px-3 py-2">Nominal</th>
+                </tr>
+            </thead>
+            <tbody>
                 @foreach ($data as $item)
-                  <tr class="{{ $loop->even ? 'bg-gray-100' : 'bg-white' }}">
-                    <td class="border-b border-gray-300 text-blue-500 flex flex-col group">
-                      {{ $item->code }}
-                      <span class="text-blue-500 text-sm opacity-0 group-hover:opacity-100">
-                        <a href="{{ route('request.show', ['code' => $item->id]) }}" target="_blank">Cetak</a>
-                        | <a class="text-red-500" href="{{ route('request.softDelete', ['id' => $item->id]) }}">Hapus</a></span>
+                <tr class="{{ $loop->even ? 'bg-gray-100' : 'bg-white' }}">
+                    <td class="border-b border-gray-300 px-3 py-2 text-blue-500 flex flex-col group">
+                        {{ $item->code }}
+                        <span class="text-blue-500 text-sm opacity-0 group-hover:opacity-100">
+                            <a href="{{ route('request.show', ['code' => $item->id]) }}" target="_blank">Cetak</a>
+                            | <a class="text-red-500" href="{{ route('request.softDelete', ['id' => $item->id]) }}">Hapus</a></span>
                     </td>
-                    <td class="border-b border-gray-300">{{ $item->received_from }}</td>
-                    <td class="border-b border-gray-300">Rp {{ number_format($item->payment_number, 0, ',' , '.' )  }}</td>
-                  </tr>
+                    <td class="border-b border-gray-300 px-3 py-2">{{ $item->received_from }}</td>
+                    <td class="border-b border-gray-300 px-3 py-2">Rp {{ number_format($item->payment_number, 0, ',' , '.' ) }}</td>
+                </tr>
                 @endforeach
-              </tbody>
-          </table>
+            </tbody>
+        </table>
         </div>
         <div class="mt-3">
               {{ $data->links() }}
